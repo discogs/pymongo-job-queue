a945a650649fd374b26e995e8c97344e36b1e663
accraze <accraze@gmail.com>
2015-11-23 08:40:21 -0800

    adding tests for iterator and next

--- test.py
+++ test.py
@@ -6,6 +6,15 @@ host = 'localhost'
 port = 27017
 pair = '%s:%d' % (host, port)
 
+class Job(object):
+
+    def __init__ (self, job_data):
+        self.job_data = job_data
+
+    def execute(self):
+        """ Returns the job's message.  """
+        return (self.job_data['data']['message'])
+
 
 class TestJobQueue(unittest.TestCase):
     @classmethod
@@ -20,7 +29,27 @@ class TestJobQueue(unittest.TestCase):
 
     def test_publish(self):
         jq = JobQueue(self.db)
-        self.assertRaises(Exception, jq.next)
         job = {'message': 'hello world!'}
         jq.pub('test', job)
         self.assertTrue(jq.pub('test', job))
+
+    def test_next(self):
+        jq = JobQueue(self.db)
+        job = {'message': 'hello world!'}
+        row = jq.next()
+        for data in row:
+            job = Job(data)
+            result = job.execute()
+            self.assertEquals(result, 'hello world!')
+
+    def test_iter(self):
+        jq = JobQueue(self.db)
+        for job in jq:
+            if job:
+                self.assertTrue(True, "Found job")
+                return
+        self.assertEquals(False, "No jobs found!")
+
+
+if __name__ == '__main__':
+    unittest.main()
