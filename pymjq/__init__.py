d75cad5339ddad7801e2af790220e0da0ad85593
accraze <accraze@gmail.com>
2016-01-06 16:24:07 -0800

    Added setup files for pypi deploy

--- pymjq/__init__.py
+++ pymjq/__init__.py
@@ -1,1 +1,8 @@
+"""Creates a queue, iterates through docs and waits when queue is empty."""
+
+__version__ = '1.0.0'
+__url__ = 'https://github.com/discogs/pymongo-job-queue'
+__author__ = 'Andy Craze'
+__email__ = 'acraze@discogsinc.com'
+
 from jobqueue import JobQueue
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pymjq',
+  packages = ['pymjq'], # this must be the same as the name above
+  version = '1.0.0',
+  description = 'Simple MongoDB based job queue',
+  author = 'Andy Craze',
+  author_email = 'acraze@discogsinc.com',
+  url = 'https://github.com/discogs/pymongo-job-queue', # use the URL to the github repo
+  download_url = 'https://github.com/discogs/pymongo-job-queue/tarball/1.0.0',
+  keywords = ['queue', 'pymongo', 'mongodb', 'job', 'async', 'worker', 'tail'], # arbitrary keywords
+  classifiers = [],
+)
